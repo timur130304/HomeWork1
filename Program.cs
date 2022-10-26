6cb9649e51d0ed18718db7ba3363fb51cbac7909
Timur Aslanov <timurckaaslanov@yandex.ru>
2022-10-26 20:21:28 +0300

    HomeWork

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 19
+
+// void Palindrome(int num) {
+//     if (num / 10000 == num % 10 && (num / 10) % 10 == (num / 1000) % 10)
+//         Console.WriteLine("The number is a palindrome.");
+//     else
+//         Console.WriteLine("The number is not a palindrome.");
+// }
+// Console.WriteLine("Enter a five-digit number");
+//     int number = Convert.ToInt32(Console.ReadLine());
+// Palindrome(number);
+
+// Задача 21
+
+// void Distance(int num1, int num2, int num3, int num4, int num5, int num6){
+//     double d  = Math.Sqrt((num4 - num1) * (num4 - num1) + (num5 - num2) * (num5 - num2) + (num6 - num3) * (num6 - num3));
+//     Console.WriteLine($"Distance: {d}");
+// }
+// Console.WriteLine("Enter the coordinates of point x1: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter the coordinates of point y1: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter the coordinates of point z1: ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter the coordinates of point x2: ");
+// int number4 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter the coordinates of point y2: ");
+// int number5 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter the coordinates of point z2: ");
+// int number6 = Convert.ToInt32(Console.ReadLine());
+// Distance(number1, number2, number3, number4, number5, number6);
+
+// Задача 23
+
+// void CubesNumbers(int num){
+//     for (int i = 1; i <= num; i++)
+//         Console.WriteLine($"Cube of the number {i} = {i * i * i}");
+// }
+// Console.WriteLine("Enter a number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// CubesNumbers(number);
